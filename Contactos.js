236f36bf1461ca2ae51b88b454472d35101988bc
Javi1499 <42743118+Javi1499@users.noreply.github.com>
2019-05-28 18:38:58 -0500

    Duplicado

--- Contactos.js
+++
@@ -1,9 +0,0 @@
-export default class Contactos {
-    constructor(vista){
-        this._nombre = vista.nombre;
-        this._email = vista.email;
-        this._fechaN = vista.fechaN
-        this._telefono = vista.telefono
-    }
-
-} 
